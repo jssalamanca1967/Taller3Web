8a0ad1a424ae36261521a2523d13a5bfbfb22dc2
Johnathan <js.salamanca1967@uniandes.edu.co>
2016-08-22 16:21:44 -0500

    Cambios
    Cambios

--- calc_operations.js
+++ calc_operations.js
@@ -6,8 +6,17 @@ function display(value){
 }
 
 function compute(){
+	document.getElementById("display").value = "Computing";
 }
 
+function clear(){
+	var vacio;
+	document.getElementById("display").value = vacio;
+}
 
 function operation(operation){
+
+	document.getElementById("display").value = document.getElementById("display").value + operation.value;
+
+
 }
